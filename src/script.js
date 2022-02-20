3d444d774b399bdc0d4a8a65d3ad71cfeb3e8a21
Wix <2209722088@qq.com>
2022-02-20 03:23:31 +0000

    增加了导入和点击反馈功能
    1.删去了不必要的代码
    2.将gltf导入行为写入一个方法中
    3.利用raycaster添加了点击交互功能，可以产生反馈!!!

--- src/script.js
+++ src/script.js
@@ -15,49 +15,45 @@ const gui = new dat.GUI()
 const canvas = document.querySelector('canvas.webgl')
 
 //fog
-const fog = new THREE.Fog('skyblue', 1, 100)
+const fog = new THREE.Fog('skyblue', 0.1, 40)
 
 // Scene
 const scene = new THREE.Scene()
-//scene.fog = fog
+scene.fog = fog
+
+//Plane
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(100, 100),
+    new THREE.MeshStandardMaterial()  
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = 0
+scene.add(plane)
 
 /**
  * Models
  */
 const gltfLoader = new GLTFLoader()
 
-//加载有动画效果的gltf文件
-let mixer = null
-gltfLoader.load(
-    '/models/Fox/glTF/Fox.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(0.025, 0.025, 0.025)
-        scene.add(gltf.scene)
-        gltf.scene.position.x= 5
-        //Animation
-        mixer = new THREE.AnimationMixer(gltf.scene)
-        const action = mixer.clipAction(gltf.animations[2])
-        action.play()
-    }
-)
-
 //加载模型文件 
 //.push()添加至末尾 .pop()删除末尾 .unshift()添加至开头 .splice()切片
 //moudelMessage[路径，长，宽，高，X，Y，Z]
+//objectGroup储存模型对象，需要注意里面还有一层scene对象
+//numberOfObjects 用来储存所有的对象，要加上非gltf导入对象，在刷新部分有用
 var modelsMessage = []
-modelsMessage.push('/models/cctv/CCTV.gltf',0.3,0.3,0.3,-5,0,0)
-modelsMessage.push('/models/car/scene.gltf',1,1,1,0,0,-10)
-modelsMessage.push('/models/test/column.gltf',1,1,1,10,0,0)
-modelsMessage.push('/models/test/notice-board.gltf',1,1,1,-15,0,0)
-modelsMessage.push('/models/test/rubbish-bin.gltf',0.3,0.3,0.3,0,0,5)
+var objectGroup = new THREE.Group()
+var numberOfObjects = 6
+modelsMessage.push('/models/cctv/CCTV.gltf',0.3,0.3,0.3,-3,0.5,-4)
+modelsMessage.push('/models/car/scene.gltf',1,1,1,0,0,-4)
+modelsMessage.push('/models/test/column.gltf',0.3,0.3,0.3,-5,2,2)
+modelsMessage.push('/models/test/notice-board.gltf',0.3,0.3,0.3,-5,2,-1.5)
+modelsMessage.push('/models/test/rubbish-bin.gltf',0.3,0.3,0.3,-3,0.8,-2)
 loadModels(modelsMessage)
 
 
 
 function loadModels(modelsMessage){
     const numbers  = modelsMessage.length/7
-    //console.log(numbers)
     for(var i = 0 ; i < numbers; i++){
         const x = i * 7
         gltfLoader.load(
@@ -71,123 +67,30 @@ function loadModels(modelsMessage){
                 gltf.scene.position.x=modelsMessage[4+x]
                 gltf.scene.position.y=modelsMessage[5+x]
                 gltf.scene.position.z=modelsMessage[6+x]
-                scene.add(gltf.scene)
-                
+                //为每个对象进行命名编号，注意只能用字符串类型
+                gltf.scene.name = x/7 + 11000 + ''
+                objectGroup.add(gltf.scene)
+                //console.log(objectGroup.getObjectByName('11001'))               
             }
         )
     }
 }
 
-
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-
-const bricksColor = textureLoader.load('/textures/bricks/color.jpg')
-const bricksAmb = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
-const bricksNormal = textureLoader.load('/textures/bricks/normal.jpg')
-const bricksRough = textureLoader.load('/textures/bricks/roughness.jpg')
-
-const grassColor = textureLoader.load('/textures/grass/color.jpg')
-const grassAmb = textureLoader.load('/textures/grass/ambientOcclusion.jpg')
-const grassNormal = textureLoader.load('/textures/grass/normal.jpg')
-const grassRough = textureLoader.load('/textures/grass/roughness.jpg')
-
-grassAmb.repeat.set(8, 8)
-grassColor.repeat.set(8, 8)
-grassNormal.repeat.set(8,8)
-grassRough.repeat.set(8, 8)
-
-grassAmb.wrapS = THREE.RepeatWrapping
-grassColor.wrapS = THREE.RepeatWrapping
-grassNormal.wrapS = THREE.RepeatWrapping
-grassRough.wrapS = THREE.RepeatWrapping
-
-grassAmb.wrapT = THREE.RepeatWrapping
-grassColor.wrapT = THREE.RepeatWrapping
-grassNormal.wrapT = THREE.RepeatWrapping
-grassRough.wrapT = THREE.RepeatWrapping
-
-/**
- * House
- */
-const house = new THREE.Group()
-scene.add(house)
-//walls
-const walls = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(4,2.5,4),
-    new THREE.MeshStandardMaterial({
-        map: bricksColor,
-        aoMap: bricksAmb,
-        normalMap: bricksNormal,
-        roughnessMap: bricksRough
-    })
+//加载有动画效果的gltf文件
+let mixer = null
+gltfLoader.load(
+    '/models/Fox/glTF/Fox.gltf',
+    (gltf) =>
+    {   
+        gltf.scene.scale.set(0.025, 0.025, 0.025)
+        gltf.scene.position.set(3,0,-3)
+        //Animation
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer.clipAction(gltf.animations[2])
+        action.play()
+        scene.add(gltf.scene)
+    }
 )
-walls.geometry.setAttribute(
-    'uv2',
-    new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2)
-)
-walls.position.y = 1.25
-house.add(walls)
-// roof
-const roof = new THREE.Mesh(
-    new THREE.ConeBufferGeometry(3.5, 1, 4),
-    new THREE.MeshStandardMaterial({color:'#b35f45'})
-)
-roof.rotation.y = Math.PI * 0.25
-roof.position.y = 3
-house.add(roof)
-//door
-const door = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(2.2, 2.2, 100, 100),
-    new THREE.MeshStandardMaterial({
-        map: doorColorTexture,
-        transparent: true,
-        alphaMap: doorAlphaTexture,
-        aoMap: doorAmbTexture,
-        displacementMap: doorHeightTexture,
-        displacementScale: 0.1,
-        normalMap: doorNormalTexture,
-        metalnessMap: doorMetalnessTexture,
-        roughnessMap: doorRoughnessTexture
-    })
-)
-door.geometry.setAttribute(
-    'uv2',
-    new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
-)
-door.position.y = 1
-door.position.z = 2.001
-house.add(door)
-house.position.z = -5
-
-
-// Floor
-const floor = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(100, 100),
-    new THREE.MeshStandardMaterial({ 
-        map: grassColor,
-        aoMap: grassAmb,
-        normalMap: grassNormal,
-        roughnessMap: grassRough
-     })
-    
-)
-floor.geometry.setAttribute(
-    'uv2',
-    new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
-)
-floor.rotation.x = - Math.PI * 0.5
-floor.position.y = 0
-scene.add(floor)
 
 /**
  * Lights
@@ -205,11 +108,6 @@ gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
-
-//doorLight 
-const doorLight = new  THREE.PointLight('#ff7d46', 1, 7)
-doorLight.position.set(0, 2.2, 2.7)
-house.add(doorLight)
 
 /**
  * Sizes
@@ -241,14 +139,15 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 500)
 camera.position.x = 4
 camera.position.y = 2
-camera.position.z = 5
+camera.position.z = 4
 scene.add(camera)
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 //控制摄像机保持在水平面以上
-controls.maxPolarAngle = Math.PI * 0.5 - 0.1
+//controls.maxPolarAngle = Math.PI * 0.5 - 0.1
 
 /**
  * Renderer
@@ -263,16 +162,45 @@ renderer.setClearColor('skyblue')
 //shadows
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
-
 moonLight.castShadow = true
-doorLight.castShadow = true
 
-walls.castShadow = true
-floor.receiveShadow = true
 
-doorLight.shadow.mapSize.width = 256
-doorLight.shadow.mapSize.height = 256
-doorLight.shadow.camera.far = 7
+//测试物体
+const object1 = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.y = 0.5
+object1.name = '001'
+objectGroup.add(object1)
+objectGroup.name = 'papa'
+//异常重要,只有在场景加载后，才能在其他地方查到对象
+scene.add(objectGroup)
+
+/**
+ * Raycaster
+ */
+ const raycaster = new THREE.Raycaster()
+ let currentIntersect = null
+
+/**
+ * Mouse
+ */
+ const mouse = new THREE.Vector2()
+ window.addEventListener('mousemove', (event) =>
+ {
+     mouse.x = event.clientX / sizes.width * 2 - 1
+     mouse.y = - (event.clientY / sizes.height) * 2 + 1
+ })
+ 
+ //点击触发事件
+ window.addEventListener('click', () =>
+ {
+    if(currentIntersect)
+    {
+        object1.material.color.set(0xFFFFFF*Math.random())
+    }
+ })
 
 
 /**
@@ -281,15 +209,47 @@ doorLight.shadow.camera.far = 7
 const clock = new THREE.Clock()
 let previousTime = 0
 
+
 const tick = () =>
 {
+    var x = 0
+    x = objectGroup.children.length 
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+    //初始化射线方向
+    raycaster.setFromCamera(mouse, camera)
+    
+    //很重要！！！！因为从gltf载入group需要时间，所以需要分情况来进行刷新帧率
+    if (x < numberOfObjects){
+        const objectsToTest = [  
+            objectGroup.getObjectByName('001'),        
+        ]
+        var intersects = raycaster.intersectObjects(objectsToTest, true)
+
+    }else if(x = numberOfObjects){
+        const objectsToTest = [
+            objectGroup.getObjectByName('001'),
+            objectGroup.getObjectByName('11000'),
+            objectGroup.getObjectByName('11001'),
+            objectGroup.getObjectByName('11002'),
+            objectGroup.getObjectByName('11003'),
+            objectGroup.getObjectByName('11004')
+        ]
+        var intersects = raycaster.intersectObjects(objectsToTest, true)
+
+    }
+    
+    //存放相交的第一个物体
+    if(intersects.length){
+        currentIntersect = intersects[0]
+    }
+    else{
+        currentIntersect = null
+    }
 
     // Model animation
-    if(mixer)
-    {
+    if(mixer){
         mixer.update(deltaTime)
     }
 
---
+++ src/test.js
@@ -0,0 +1,118 @@
+//暂时用不到的一部分代码放在这里
+/**
+ * Textures
+ */
+ const textureLoader = new THREE.TextureLoader()
+ const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+ const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+ const doorAmbTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+ const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+ const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+ const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+ 
+ const bricksColor = textureLoader.load('/textures/bricks/color.jpg')
+ const bricksAmb = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
+ const bricksNormal = textureLoader.load('/textures/bricks/normal.jpg')
+ const bricksRough = textureLoader.load('/textures/bricks/roughness.jpg')
+ 
+ const grassColor = textureLoader.load('/textures/grass/color.jpg')
+ const grassAmb = textureLoader.load('/textures/grass/ambientOcclusion.jpg')
+ const grassNormal = textureLoader.load('/textures/grass/normal.jpg')
+ const grassRough = textureLoader.load('/textures/grass/roughness.jpg')
+ 
+ grassAmb.repeat.set(8, 8)
+ grassColor.repeat.set(8, 8)
+ grassNormal.repeat.set(8,8)
+ grassRough.repeat.set(8, 8)
+ 
+ grassAmb.wrapS = THREE.RepeatWrapping
+ grassColor.wrapS = THREE.RepeatWrapping
+ grassNormal.wrapS = THREE.RepeatWrapping
+ grassRough.wrapS = THREE.RepeatWrapping
+ 
+ grassAmb.wrapT = THREE.RepeatWrapping
+ grassColor.wrapT = THREE.RepeatWrapping
+ grassNormal.wrapT = THREE.RepeatWrapping
+ grassRough.wrapT = THREE.RepeatWrapping
+ 
+ /**
+  * House
+  */
+ const house = new THREE.Group()
+ scene.add(house)
+ //walls
+ const walls = new THREE.Mesh(
+     new THREE.BoxBufferGeometry(4,2.5,4),
+     new THREE.MeshStandardMaterial({
+         map: bricksColor,
+         aoMap: bricksAmb,
+         normalMap: bricksNormal,
+         roughnessMap: bricksRough
+     })
+ )
+ walls.geometry.setAttribute(
+     'uv2',
+     new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2)
+ )
+ walls.position.y = 1.25
+ house.add(walls)
+ // roof
+ const roof = new THREE.Mesh(
+     new THREE.ConeBufferGeometry(3.5, 1, 4),
+     new THREE.MeshStandardMaterial({color:'#b35f45'})
+ )
+ roof.rotation.y = Math.PI * 0.25
+ roof.position.y = 3
+ house.add(roof)
+ //door
+ const door = new THREE.Mesh(
+     new THREE.PlaneBufferGeometry(2.2, 2.2, 100, 100),
+     new THREE.MeshStandardMaterial({
+         map: doorColorTexture,
+         transparent: true,
+         alphaMap: doorAlphaTexture,
+         aoMap: doorAmbTexture,
+         displacementMap: doorHeightTexture,
+         displacementScale: 0.1,
+         normalMap: doorNormalTexture,
+         metalnessMap: doorMetalnessTexture,
+         roughnessMap: doorRoughnessTexture
+     })
+ )
+ door.geometry.setAttribute(
+     'uv2',
+     new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
+ )
+ door.position.y = 1
+ door.position.z = 2.001
+ house.add(door)
+ house.position.z = -5
+ 
+ 
+ // Floor
+ const floor = new THREE.Mesh(
+     new THREE.PlaneBufferGeometry(100, 100),
+     new THREE.MeshStandardMaterial({ 
+         map: grassColor,
+         aoMap: grassAmb,
+         normalMap: grassNormal,
+         roughnessMap: grassRough
+      })
+     
+ )
+ floor.geometry.setAttribute(
+     'uv2',
+     new THREE.Float32BufferAttribute(floor.geometry.attributes.uv.array, 2)
+ )
+ floor.rotation.x = - Math.PI * 0.5
+ floor.position.y = 0
+ scene.add(floor)
+ //doorLight 
+const doorLight = new  THREE.PointLight('#ff7d46', 1, 7)
+doorLight.position.set(0, 2.2, 2.7)
+house.add(doorLight)
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+doorLight.castShadow = true
